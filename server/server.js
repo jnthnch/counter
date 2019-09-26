65a1551d4ac66ab033c192eea24339d64730a720
jnthnch <jchu127@gmail.com>
2019-09-26 14:36:47 -0700

    Add jsonwebtoken

--- server/server.js
+++ server/server.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const path = require('path');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
 app.use('/', express.static(path.join(__dirname, '../public')));
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'GET worked',
+  })
+})
+
+app.post('/api', (req, res) => {
+  res.json({
+    message: 'POST worked'
+  })
+})
+
+app.post('/api/login', (req, res) => {
+  const user = {
+    id: 1,
+    username: 'jon'
+  }
+
+  jwt.sign(user, 'secret', (err, token) => {
+    res.json({
+      token
+    })
+  })
+})
+
 
 app.listen(app.get('port'), () => {
   console.log(`Server started: http://localhost:${app.get('port')}/`);
